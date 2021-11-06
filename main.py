14e2443d10469b6f562434499b985338c98427b9
KamalDGRT <kamaldgrt@gmail.com>
2021-11-06 23:14:13 +0530

    learnt how to validate data using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 # https://fastapi.tiangolo.com/tutorial/first-steps/
 
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Post(BaseModel):
+    """
+    A pydantic model that does the part of data
+    validation.
+
+    It is because of this we can ensure that whatever
+    data is sent by the frontend is in compliance
+    with the backend.
+    """
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 app = FastAPI()
 
@@ -12,13 +29,15 @@ async def root():
 
 
 # This is the content that I gave in the Body -> Raw -> JSON in the Postman
+#  Feel free to test out with vallues of different types.
 # {
 #     "title": "Yii2 Framework",
-#     "content": "Yii2 is one of the top 5 PHP Frameworks"
+#     "content": "Yii2 is one of the top 5 PHP Frameworks",
+#     "rating": 4
 # }
 @app.post('/createpost')
-def create_post(payLoad: dict = Body(...)):
-    print(payLoad)
+def create_post(post: Post):
+    post.dict()   # Converts a pydantic model to a dictionary
     return {
-        "new_post": f"Title: {payLoad['title']}, Content: {payLoad['content']}"
+        "data": post
     }
