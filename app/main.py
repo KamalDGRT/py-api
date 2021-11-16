9af5c4383ad35dec0e9b54501ce9166595eb0c4d
KamalDGRT <kamaldgrt@gmail.com>
2021-11-16 12:40:25 +0530

    implement user fetch

--- app/main.py
+++ app/main.py
@@ -187,3 +187,11 @@ def create_post(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@app.get('/user/{id}', response_model=schemas.UserOut) 
+def get_post(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {id} does not exist!")
+    return user
