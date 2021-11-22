edadee8e0259861a1814a38e83c6a4bc44c9df2a
KamalDGRT <kamaldgrt@gmail.com>
2021-11-22 18:29:49 +0530

    add config for cors

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 # How to run the code: uvicorn app.main:app --reload
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .config import settings
@@ -11,6 +12,16 @@ from .routers import post, user, auth, vote
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
